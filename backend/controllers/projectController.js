051e40619daa01378a1a3990844dacd9cce4a3b5
YingCG <yingchiugoh@gmail.com>
2022-08-29 10:30:52 +1200

    rest api

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -22,21 +22,39 @@ const setProjects = asyncHandler( async (req, res) => {
         // .json({ message: "Please add a text field"})
         throw new Error( "Please add a text field") // this come with lot of errors. To do this we can add the error middleware
     }
-    res.status(200).json({message: 'Set projects'})
+    const project = await Project.create({
+        text: req.body.text
+    })
+    res.status(200).json(project)
 })
 
 // @describe Update goals
 // @route    PUT /api/projects/:id
 // @access   Private
 const updateProject = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Update project ${req.params.id}`})
+    const project = await Project.findById(req.params.id)
+
+    if(!project){
+        res.status(400)
+        throw new Error("Project not found")
+    }
+
+    const updatedProject = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true, })
+    res.status(200).json(updatedProject)
 })
 
 // @describe Delete goals
 // @route    DELETE /api/projects/:id
 // @access   Private
 const deleteProject = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Delete project ${req.params.id}`})
+    const project = await Project.findById(req.params.id)
+
+    if(!project){
+        res.status(400)
+        throw new Error('Project not found')
+    }
+    await project.remove()
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
